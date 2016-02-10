5da58927636b04899dcbccae075e1781faa4ab6a
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-02-10 12:51:29 +0900

    gebenを追加

---
+++ .emacs.d/elpa/geben-20100330.343/geben-pkg.el
@@ -0,0 +1,5 @@
+(define-package "geben" "20100330.343" "DBGp protocol frontend, a script debugger" 'nil :url "http://code.google.com/p/geben-on-emacs/" :keywords
+  '("dbgp" "debugger" "php" "xdebug" "perl" "python" "ruby" "tcl" "komodo"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
