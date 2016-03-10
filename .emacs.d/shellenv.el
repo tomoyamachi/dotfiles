5cb14456b8d1e7559bb9a312b860cc3987050b5d
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-03-10 12:39:52 +0900

    ggtagsを追加

---
+++ .emacs.d/shellenv.el
@@ -0,0 +1,1 @@
+(setenv "PATH" "/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")
