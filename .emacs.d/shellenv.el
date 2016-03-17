b1ee492bac843a492228a461d1affb156816d536
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-03-17 17:25:58 +0900

    cucumberの導入に際して行ったこと

--- .emacs.d/shellenv.el
+++ .emacs.d/shellenv.el
@@ -1,1 +1,1 @@
-(setenv "PATH" "/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")
+(setenv "PATH" "/usr/local/bin:/usr/local/sbin:/Users/amachi/dotfiles/shells:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin")
