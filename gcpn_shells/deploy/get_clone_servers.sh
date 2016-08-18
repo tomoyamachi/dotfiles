b497cd28de17cde14f000a66e9bbe80ce8024c54
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-08-18 11:47:09 +0900

    Delete old files

--- gcpn_shells/deploy/get_clone_servers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" ]; then
-    TAGNAME=$1
-else
-    echo "デプロイしたいAutoScailingServerのTag : Nameのvalueを入れてください"
-    exit 1
-fi
-aws ec2 describe-instances --filters "Name=tag:Name,Values=$TAGNAME"| jq '.Reservations | .[]' | jq '.Instances | .[]' | jq '.PrivateIpAddress'
